e4bb620f865e6a81ac93e4d85e5a4b42217d2eb5
Victor Lohan de Souza Silva <victor.lohan@academico.ifrn.edu.br>
2023-12-27 16:37:21 -0300

    Inicializando Projeto

---
+++ src/main/java/com/ifrn/sge/SgeApplication.java
@@ -0,0 +1,13 @@
+package com.ifrn.sge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifrn/sge/SgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrn.sge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
