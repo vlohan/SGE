d690cbdc5b52cee451fc7ff6b67761b2b9ea9357
vlohan <v.lohansilva@gmail.com>
2024-01-01 17:38:57 -0300

    Modelos e ParkController

---
+++ src/main/java/com/ifrn/sge/controllers/ParkController.java
@@ -0,0 +1,11 @@
+package com.ifrn.sge.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/vaga")
+public class ParkController {
+
+}
---
+++ src/main/java/com/ifrn/sge/models/Park.java
@@ -0,0 +1,55 @@
+package com.ifrn.sge.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+public class Park implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @NotNull
+    @NotBlank
+    private String id;
+    @OneToOne
+    private Car client;
+
+    public Park() {
+    }
+
+    public Park(String id, Car client) {
+        this.id = id;
+        this.client = client;
+    }
+
+    @Override
+    public String toString() {
+        return "Park{" +
+                "id='" + id + '\'' +
+                ", client=" + client +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Car getClient() {
+        return client;
+    }
+
+    public void setClient(Car client) {
+        this.client = client;
+    }
+}
