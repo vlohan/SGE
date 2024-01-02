5f4077c3d3beb1a5e1971894ec714af2aa72decf
Vitor Gabriel de Medeiros Bilro <v.lohansilva@gmail.com>
2024-01-01 21:02:57 -0300

    Corrigindo páginas e iniciando SpringSecurity

--- src/main/java/com/ifrn/sge/controllers/ParkController.java
+++ src/main/java/com/ifrn/sge/controllers/ParkController.java
@@ -4,10 +4,15 @@ import com.ifrn.sge.models.Park;
 import com.ifrn.sge.repositories.ParkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,5 +33,23 @@ public class ParkController {
     public ModelAndView listAllPark() {
         List<Park> opt = pr.findAll();
         return new ModelAndView("park/listAllPark").addObject("parks", opt);
+    }
+
+    @GetMapping("/adicionar")
+    public ModelAndView addPage(Park park) {
+        return new ModelAndView("park/addPage.html").addObject("park", park);
+    }
+
+    @PostMapping("/adicionar")
+    public ModelAndView addPark(@Valid @ModelAttribute("park") Park park, BindingResult br, RedirectAttributes ra) {
+
+        if(br.hasErrors()) {
+            return addPage(park);
+        }
+
+        pr.save(park);
+        ra.addFlashAttribute("mensagem", "Vaga salva com sucesso");
+        return new ModelAndView("redirect:/vaga/listar");
+
     }
 }
---
+++ src/main/java/com/ifrn/sge/models/User.java
@@ -0,0 +1,120 @@
+package com.ifrn.sge.models;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import javax.management.relation.Role;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User implements UserDetails, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private List<Car> cars;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String name;
+
+    @ManyToMany
+    @Lazy(value = false)
+    private List<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Car> getRegistration() {
+        return cars;
+    }
+
+    public void setRegistration(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return (Collection<? extends GrantedAuthority>) this.roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", cars=" + cars + ", password=" + password + ", name=" + name
+                + ", roles=" + roles + "]";
+    }
+
+}
